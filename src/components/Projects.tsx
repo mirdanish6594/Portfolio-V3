1ed2d401314375a5523595b19f168aa5d7f3ebde
Danish Mir <76698292+mirdanish6594@users.noreply.github.com>
2024-12-24 14:49:47 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,28 +7,28 @@ const projects = [
   {
     title: "HR Dashboard",
     description: "Created an HR dashboard in Tableau, providing insightful visualizations and data-driven metrics for workforce analysis and decision-making.",
-    image: "Assets/images/hr-dashboard.png",
+    image: "Assets/images/hr-dashboard.PNG",
     tags: ["Python", "Flask", "OpenCV", "PostgreSQL"],
     link: "https://public.tableau.com/app/profile/danish.mir1812/viz/HRDashboardProject_17221797463460/HRSummary"
   },
   {
     title: "ReactorAnalyzer",
     description: "Interactive dashboard for visualizing and analyzing chemical reactor performance using Python and Streamlit.",
-    image: "Assets/images/reactor.png",
+    image: "Assets/images/reactor.PNG",
     tags: ["Python", "Pandas", "Streamlit", "Plotly"],
     link: "https://github.com/mirdanish6594/ReactorAnalyzer"
   },
   {
     title: "Snake Game AI",
     description: "Reinforcement learning project implementing Q-learning for an intelligent Snake game agent. And plots showing the agents learning curve.",
-    image: "Assets/images/snake.png",
+    image: "Assets/images/snake.PNG",
     tags: ["Python", "Q-Learning", "AI", "PyGame", "Matplotlib"],
     link: "https://github.com/mirdanish6594/SnakeAI-ReinforcementLearning"
   },
   {
     title: "Light Monitoring System",
     description: "A real-time monitoring system for street lights. It provides continuous monitoring of leakage current and earthing status for electrical poles.",
-    image: "Assets/images/iot.png",
+    image: "Assets/images/iot.PNG",
     tags: ["React", "TypeScript", "Vite", "Firebase", "Leaflet Maps", "TailwindCSS"],
     link: "https://github.com/mirdanish6594/IOT-Dashboard"
   }
